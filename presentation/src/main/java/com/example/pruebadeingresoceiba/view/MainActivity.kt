be5c094c12ee14268db0ac6715b3f945e268c0fe
Dixon Fabian Salcedo Guecha <df.salcedo@mensajerosurbanos.com>
2023-03-02 00:57:04 -0500

    First commit with dagger hilt and distribution of modules

--- presentation/src/main/java/com/example/pruebadeingresoceiba/view/MainActivity.kt
+++ presentation/src/main/java/com/example/pruebadeingresoceiba/view/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.example.pruebadeingresoceiba
+package com.example.pruebadeingresoceiba.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pruebadeingresoceiba.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
